f7f79385e63168a5d0a9371f291b27f7a3a2953d
redanula <yanglangjing@hotmail.com>
2020-05-01 00:32:43 +0800

    202. 快乐数

---
+++ 202. 快乐数.playground/Contents.swift
@@ -0,0 +1,31 @@
+func isHappy(_ n: Int) -> Bool {
+    var dic: [Int:Int] = [:]
+    var sum = sumAll(n)
+    
+    while true {
+        if sum == 1 {
+            break
+        }
+        else if dic[sum] == 1 {
+            break
+        }
+        dic[sum] = 1
+        sum = sumAll(sum)
+    }
+    
+    return sum == 1 ? true : false
+}
+
+func sumAll(_ m: Int) -> Int {
+    var sum = 0
+    var i = 0
+    var s = m
+    while(s > 0) {
+        i = s % 10
+        sum += i * i
+        s = (s - i) / 10
+    }
+    return sum
+}
+
+var ref = isHappy(19)
