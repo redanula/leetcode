f8c8bd9869c2ea7bd361d0b08d1b7cf01dc01ef2
redanula <yanglangjing@hotmail.com>
2020-05-05 23:12:29 +0800

    q: 3. 无重复字符的最长子串

--- leetcode-swift-redan.playground/Pages/3. 无重复字符的最长子串.xcplaygroundpage/Contents.swift
+++ leetcode-swift-redan.playground/Pages/3. 无重复字符的最长子串.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
+/*
+    3. 无重复字符的最长子串
+    First / Best
+    关键：滑动窗口
+    时间复杂度：O(n)
+    空间复杂度：O(m), m为字符可出现个数
+*/
 
-import Foundation
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    
+    var start = 0, end = 0, result = 0
+    let charsArray = Array(s)
+    let length = charsArray.count
+    var hashValue:[Character: Int] = [:]
+    
+    while start < length && end < length {
+        
+        let currentChat = charsArray[end]
+        
+        if let cacheIndex = hashValue[currentChat] {
+            start = max(start, cacheIndex)
+        }
+        end = end + 1
+        result = max(result, end - start)
+        hashValue[currentChat] = end
+    }
+    return result
+}
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+let a = lengthOfLongestSubstring("abac")
+
