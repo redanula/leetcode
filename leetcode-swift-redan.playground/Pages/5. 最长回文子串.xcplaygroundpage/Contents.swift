19d57dbb9a9d83010ea9599c82af1df3e96b97f4
redanula <yanglangjing@hotmail.com>
2020-05-21 22:13:10 +0800

    fix: 5. 最长回文子串

--- leetcode-swift-redan.playground/Pages/5. 最长回文子串.xcplaygroundpage/Contents.swift
+++ leetcode-swift-redan.playground/Pages/5. 最长回文子串.xcplaygroundpage/Contents.swift
@@ -1,37 +1,39 @@
 /*
     5. 最长回文子串
-    First
-    关键：动态规划
+    Best
+    关键：动态规划->中心扩展
     时间复杂度：O(n2)
-    空间复杂度：O(n2)
+    空间复杂度：O(n2)->O(1)
 */
 
 func longestPalindrome(_ s: String) -> String {
-    
-    var ans = ""
     let chars = Array(s)
-    var dp = Array(repeating: Array(repeating: false, count: chars.count), count: chars.count)
-    for l in 0..<chars.count {
-        for i in 0..<chars.count - l {
-            let j = i + l
-            if l == 0 {
-                dp[i][j] = true
-            }
-            else if l == 1 {
-                dp[i][j] = chars[i] == chars[j]
-            }
-            else {
-                dp[i][j] = chars[i] == chars[j] && dp[i+1][j-1]
-            }
-
-            if dp[i][j] && l + 1 > ans.count {
-                ans = String(chars[i...j])
-            }
-        }
+    guard chars.count > 0 else {
+        return ""
     }
     
-    
-    return ans
+    var start = 0
+    var end = 0
+    for i in 0..<chars.count {
+        let len1 = expendString(chars, start: i, end: i)
+        let len2 = expendString(chars, start: i, end: i+1)
+        let len = max(len1, len2)
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return String(chars[start..<end+1])
 }
 
-let r = longestPalindrome("aa")
+func expendString(_ ss: [Character], start:Int, end:Int) -> Int {
+    var left = start
+    var right = end
+    while left >= 0 && right < ss.count && ss[left] == ss[right] {
+        left = left - 1
+        right = right + 1
+    }
+    return right - left - 1
+}
+
+let r = longestPalindrome("babad")
