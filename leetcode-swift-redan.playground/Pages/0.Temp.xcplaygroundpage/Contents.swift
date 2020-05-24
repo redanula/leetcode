7f65704aafdae1583161ce53db383ed55b8a56b0
redanula <yanglangjing@hotmail.com>
2020-05-24 20:42:51 +0800

    jz-04. 二维数组中的查找

--- leetcode-swift-redan.playground/Pages/0.Temp.xcplaygroundpage/Contents.swift
+++ leetcode-swift-redan.playground/Pages/0.Temp.xcplaygroundpage/Contents.swift
@@ -5,3 +5,52 @@
     时间复杂度：
     空间复杂度：
 */
+
+/*
+// 二维数组
+let input = [
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+
+let input2 = [
+  [1,1]
+]
+ 
+
+// 链表
+ 
+let l1 = ListNode(2)
+let l2 = ListNode(4)
+let l3 = ListNode(3)
+l1.next = l2
+l2.next = l3
+
+let r1 = ListNode(5)
+let r2 = ListNode(6)
+let r3 = ListNode(4)
+r1.next = r2
+r2.next = r3
+
+var ans = addTwoNumbers(l1,r1)
+while ans != nil {
+    print(ans!.val)
+    ans = ans?.next
+}
+
+// 树
+let a = TreeNode.init(2)
+let al = TreeNode.init(1)
+let ar = TreeNode.init(3)
+let all = TreeNode.init(4)
+let alr = TreeNode.init(5)
+a.left = al
+a.right = ar
+al.left = all
+al.right = alr
+let r = levelOrder(a)
+
+*/
