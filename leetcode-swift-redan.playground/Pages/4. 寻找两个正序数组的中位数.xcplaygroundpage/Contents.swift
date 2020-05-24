80f6b5ec14a2d96a74f828c4649f941dd0ae909d
redanula <yanglangjing@hotmail.com>
2020-05-24 13:13:51 +0800

    q: 4. 寻找两个正序数组的中位数 tips

--- leetcode-swift-redan.playground/Pages/4. 寻找两个正序数组的中位数.xcplaygroundpage/Contents.swift
+++ leetcode-swift-redan.playground/Pages/4. 寻找两个正序数组的中位数.xcplaygroundpage/Contents.swift
@@ -11,18 +11,19 @@ func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
     let len1 = nums1.count
     let len2 = nums2.count
     
-    // 奇数则x
+    // 奇数则x，如11则找k=6
     if (len1 + len2)%2 == 1 {
-        let middle = (len1 + len2)/2
-        let median = find(nums1, nums2, middle + 1)
+        let k = (len1 + len2)/2
+        let median = find(nums1, nums2, k + 1)
         return Double(median)
-    } else { // 偶数则x,x+1
-        let middle = (len1 + len2)/2
-        let median = Double(find(nums1, nums2, middle) + find(nums1, nums2, middle + 1)) / 2.0
+    } else { // 偶数则k,k+1,如10则找k=5,6
+        let k = (len1 + len2)/2
+        let median = Double(find(nums1, nums2, k) + find(nums1, nums2, k + 1)) / 2.0
         return median
     }
 }
 
+// 找到第k小的数
 func find(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> Int{
     
     let slen1 = nums1.count
@@ -33,21 +34,26 @@ func find(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> Int{
     repeat {
         
         // 边界
+        // slen1遍历完了；index1的范围[0, slen1 - 1]
         if index1 == slen1 {
+            // 返回nums2的第nextK个数index = nextK - 1
             return nums2[index2 + nextK - 1]
-        } else if index2 == slen2 {
+        } else if index2 == slen2 { // slen2遍历完了；index2的范围[0, slen2 - 1]
+            // 返回nums1的第nextK个数index = nextK - 1
             return nums1[index1 + nextK - 1]
         }else if nextK == 1 {
             return min(nums1[index1], nums2[index2])
         }
         
         let middle = nextK / 2
+        // 取min做数组越界保护
         let nextIndex1 = min(index1 + middle, slen1) - 1;
         let nextIndex2 = min(index2 + middle, slen2) - 1;
         
         if nums1[nextIndex1] <= nums2[nextIndex2] {
             // 排除掉 nums1[0..k/2]
             nextK = nextK - (nextIndex1 - index1 + 1)
+            // index1从排除掉的 index1...nextIndex1 后开始
             index1 = nextIndex1 + 1
         }else {
             // 排除掉 nums2[0..k/2]
