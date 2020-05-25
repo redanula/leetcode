2cdfb2472c026bbe52ea0ff76dda4cefe10816c9
redanula <yanglangjing@hotmail.com>
2020-05-25 22:02:55 +0800

    fix: 146. LRU缓存机制

--- leetcode-swift-redan.playground/Pages/146. LRU缓存机制.xcplaygroundpage/Contents.swift
+++ leetcode-swift-redan.playground/Pages/146. LRU缓存机制.xcplaygroundpage/Contents.swift
@@ -1,46 +1,50 @@
 /*
     146. LRU缓存机制
     Best
-    关键：双向链表
+    关键：双向链表、Hash、删除节点、移动到Head
     时间复杂度：O(1)
     空间复杂度：O(n)
 */
 
-class LRUCache {
-
-    public class ListNode {
-        public var val: Int
-        public var next: ListNode?
-        public var pre: ListNode?
-        public init(_ val: Int) {
-            self.val = val
-            self.next = nil
-            self.pre = nil
-        }
+public class ListNode {
+    public var key: Int
+    public var val: Int
+    public var next: ListNode?
+    public var pre: ListNode?
+    public init(_ key: Int, _ val: Int) {
+        self.val = val
+        self.key = key
+        self.next = nil
+        self.pre = nil
     }
+}
+
+class LRUCache {
     
-    var map = [Int:ListNode]()
-    var maxLen = 0
-    var len = 0
-    var head:ListNode? = nil
-    var tail:ListNode? = nil
+    var map:[Int:ListNode]
+    var maxLen:Int
+    var len:Int
+    var head:ListNode
+    var tail:ListNode
     
     init(_ capacity: Int) {
         maxLen = capacity
         len = 0
-        head = nil
-        tail = nil
+        // 构造dummy节点
+        head = ListNode(0, 0)
+        tail = ListNode(0, 0)
+        head.next = tail
+        tail.pre = head
+        map = [Int:ListNode]()
     }
     
     func get(_ key: Int) -> Int {
-        
         if let node = map[key] {
             // 更新最近使用key
             // 把node放在head
-            moveNodeToHead(node)
-            return head!.val
+            moveToHead(node)
+            return node.val
         }
-        
         return -1
     }
     
@@ -49,58 +53,48 @@ class LRUCache {
         if let node = map[key] {
             // 把node放在head
             node.val = value
-//            print(value)
-            moveNodeToHead(node)
-            map[key] = head
+            moveToHead(node)
         } else {
-            if len == maxLen {
-                
-                if tail != nil {
-                    let oldValue = tail!.val
-//                    print(oldValue)
-                    tail!.pre?.next = nil
-                    
-                    // 释放空间
-                    map[oldValue] = nil
-                    tail = tail!.pre
-                }
-                let node = ListNode(value)
-                moveNodeToHead(node)
-                map[key] = head
-            } else {
-                len += 1
-                let node = ListNode(value)
-                moveNodeToHead(node)
-                map[key] = head
-//                print(key)
-                if tail == nil {
-//                    print("tail")
-                    tail = head
-                }
+            
+            len += 1
+            let node = ListNode(key, value)
+            addToHead(node)
+            map[key] = node
+        
+            if len > maxLen {
+                let oldNode = removeTail()
+                // 释放空间
+                map[oldNode.key] = nil
+                len -= 1
             }
         }
     }
     
-    func moveNodeToHead(_ node: ListNode) {
-        if let nextNode = node.next {
-            nextNode.pre = node.pre
-        } else {
-            tail = head
-        }
-        
-        if let preNode = node.pre {
-           preNode.next = node.next
-        }
-        
-        head?.pre = node
-        node.pre = nil
-        node.next = head
-        
-        head = node
+    func removeNode(_ node: ListNode) {
+        node.next?.pre = node.pre
+        node.pre?.next = node.next
+    }
+    
+    func addToHead(_ node: ListNode) {
+        node.pre = head
+        node.next = head.next
+        head.next?.pre = node
+        head.next = node
+    }
+    
+    func moveToHead(_ node: ListNode) {
+        removeNode(node)
+        addToHead(node)
+    }
+    
+    func removeTail() -> ListNode {
+        let node = tail.pre
+        removeNode(node!)
+        return node!
     }
 }
 
-let obj = LRUCache(1)
+let obj = LRUCache(2)
 obj.put(1, 1);
 obj.put(2, 2);
 let ret_1: Int = obj.get(1);       // 返回  1
