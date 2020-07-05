0888e540cad0fdfa5ea5e6a96b6e63738387f3f4
redanula <yanglangjing@hotmail.com>
2020-07-05 14:13:57 +0800

    q: 5453. 所有蚂蚁掉下来前的最后一刻

--- Total.playground/Pages/5453. 所有蚂蚁掉下来前的最后一刻.xcplaygroundpage/Contents.swift
+++ Total.playground/Pages/5453. 所有蚂蚁掉下来前的最后一刻.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
-//: [Previous](@previous)
+//: 5453. 所有蚂蚁掉下来前的最后一刻
 
-import Foundation
+class Solution {
+    func getLastMoment(_ n: Int, _ left: [Int], _ right: [Int]) -> Int {
+        
+        var ans = 0
+        if let m = left.max() {
+            ans = max(ans, m)
+        }
+        
+        if let m = right.min() {
+            ans = max(ans, n - m)
+        }
+        return ans
+    }
+}
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+let s = Solution()
+let r = s.getLastMoment(7, [0,1,2,3,4,5,6,7], [])
+//let r = s.getLastMoment(4, [4,3], [0,1])
