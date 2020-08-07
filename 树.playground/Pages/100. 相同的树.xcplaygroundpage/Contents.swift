ded820d1e9ffa55154ba7b85e77279e52a27b178
redanula <yanglangjing@hotmail.com>
2020-08-07 13:54:40 +0800

    q: 100.相同的树

---
+++ 树.playground/Pages/100. 相同的树.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+/*
+    First / Best
+    关键：递归
+    时间复杂度：O(min(m,n))
+    空间复杂度：O(min(m,n))
+*/
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        
+        if p == nil, q != nil {
+            return false
+        } else if p != nil, q == nil {
+            return false
+        } else if p == nil, q == nil {
+            return true
+        } else if p?.val != q?.val {
+            return false
+        } else {
+            return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        }
+    }
+}
