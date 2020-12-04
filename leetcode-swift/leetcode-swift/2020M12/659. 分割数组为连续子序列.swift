80ca37c5a8e176e1e442c81d4cc940e125047224
redanula <yanglangjing@hotmail.com>
2020-12-04 14:36:19 +0800

    proj: setting

---
+++ leetcode-swift/leetcode-swift/2020M12/659. 分割数组为连续子序列.swift
@@ -0,0 +1,21 @@
+//
+//  659. 分割数组为连续子序列.swift
+//  leetcode-swift
+//
+//  Created by ylj on 2020/12/4.
+//  Copyright © 2020 com. All rights reserved.
+//
+
+/*
+    First / Best
+    关键：
+    时间复杂度：
+    空间复杂度：
+*/
+
+class Solution {
+    func isPossible(_ nums: [Int]) -> Bool {
+        print("1")
+        return false
+    }
+}
---
+++ leetcode-swift/leetcode-swift/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  leetcode-swift
+//
+//  Created by ylj on 2020/12/4.
+//  Copyright © 2020 com. All rights reserved.
+//
+
+import Foundation
+
+let s = Solution()
+let r = s.isPossible([123])
+
+print("Hello, World!")
+
